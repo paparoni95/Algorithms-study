355fe2fdd7c037252148e325bab32a3030a5e373
Kim Do Hyun <31722512+kimdohyun14@users.noreply.github.com>
2019-08-05 20:39:02 +0900

    2019-08-05
    한글 깨짐 수정.

--- BOJ/Greedy/BOJ11047.cpp
+++ BOJ/Greedy/BOJ11047.cpp
@@ -13,21 +13,10 @@ int main() {
 		cin >> a;
 		coin.push_back(a);
 	}
-	sort(coin.begin(), coin.end(), greater<int>()); //��������
-	/*
-	int idx = 0, cnt = 0, tmp = 0;
-	while (K) {
-		if (K >= coin[idx]) {
-			tmp = K / coin[idx];
-			K = K - tmp * coin[idx];
-			cnt += tmp;
-		}
-		idx++;
-	}
-	cout << cnt;
-	*/
+	sort(coin.begin(), coin.end(), greater<int>()); //내림차순으로 정렬
+
 	int cnt = 0;
-	for (int i = 0; i < coin.size(); i++) {
+	for (int i = 0; i < coin.size(); i++) { //가장 큰 동전으로부터 나눠본다.
 		cnt += K / coin[i];
 		K %= coin[i];
 	}
