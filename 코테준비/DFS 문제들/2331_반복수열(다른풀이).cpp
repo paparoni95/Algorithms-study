9f1775ea3b5ba362fb5eba42277a8d9ba81aea46
DoHyun Kim <dohyun9565@gmail.com>
2020-07-05 02:06:47 +0900

    다른 풀이의 해답 및 풀이과정

--- 코테준비/DFS 문제들/2331_반복수열(다른풀이).cpp
+++ 코테준비/DFS 문제들/2331_반복수열(다른풀이).cpp
@@ -1,6 +1,10 @@
 // https://jaimemin.tistory.com/946
-// DFS �ε� Ǯ���� ���� �˾Ҵ�.
-// visited[num]�� �� 3���� ������ �ϼ�����?
+// DFS 로도 풀리는 것을 알았다.
+// visited[num]이 왜 3에서 끝나게 하셨을까?
+// 궁금증 해결 : visited[num] == 2에서 끝나게 되버리면, 해당 문제 예시에서 37만
+//               중첩되고 나머지는 중첩인지 아닌지 확인할 방도가 나타나지 않는다.
+//               따라서 두 번째 반복하는 시점 즉, visited[num] == 3일 때 재귀함수
+//               를 탈출하여서 문제를 해결하는 방안이다.
 
 #include <iostream>
 #include <vector>
@@ -15,7 +19,7 @@ int visited[MAX];
 void DFS(int num)
 {
 	visited[num]++;
-	// �ݺ������� �� �� �ݺ��Ǿ��ٴ� ���� �ǹ�
+	// 반복구간이 한 번 반복되었다는 것을 의미
 	if (visited[num] == 3) return;
 
 	int sum = 0;
@@ -37,7 +41,7 @@ int main()
 
 	DFS(A);
 	int result = 0;
-	//�� ���� ������ ���ڵ��� �ݺ������� ������ ���� ���ڵ�
+	//한 번만 등장한 숫자들이 반복구간에 속하지 않은 숫자들
 	for (int i = 0; i < MAX; i++)
 	{
 		if (visited[i] == 1) result++;
