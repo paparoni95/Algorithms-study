5bcf57a63093ebaedb876f6a80f523b39b067271
Kim Do Hyun <dohyun9565@gmail.com>
2020-07-19 19:17:00 +0900

    간단한 문자열 처리

---
+++ Programmers/가운데_글자_가져오기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    if (s.length() % 2 == 1)
+        answer += s[s.length() / 2];
+
+    else
+    {
+        answer += s[(s.length() - 1) / 2];
+        answer += s[(s.length() - 1) / 2 + 1];
+    }
+    return answer;
+}
