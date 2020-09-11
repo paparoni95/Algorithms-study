64092c87e39e4ab84dcb6637e19dc7e5525dfbc7
DoHyun Kim <dohyun9565@gmail.com>
2020-09-11 20:03:22 +0900

    정렬 연습

---
+++ BaekJoon/Sorting/BOJ10867.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> v(n, 0);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+	for (auto e : v) cout << e << ' ';
+	cout << '\n';
+	return 0;
+}
