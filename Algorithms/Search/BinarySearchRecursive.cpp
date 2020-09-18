de52d2f41550732f251c01c084f1aa00af238e90
DoHyun Kim <dohyun9565@gmail.com>
2020-09-19 02:06:47 +0900

    기본적인 탐색 방법

---
+++ Algorithms/Search/BinarySearchRecursive.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 이진 탐색 소스코드 구현(재귀 함수)
+int BinarySearch(vector<int>& arr, int target, int start, int end) {
+	if (start > end) return -1;
+	int mid = (start + end) / 2;
+	// 찾은 경우 중간점 인덱스 반환
+	if (arr[mid] == target) return mid;
+	// 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+	else if (arr[mid] > target) return BinarySearch(arr, target, start, mid - 1);
+	// 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+	else return BinarySearch(arr, target, mid + 1, end);
+}
+
+int n, target;
+vector<int> arr;
+
+int main() {
+	cin >> n >> target;
+	// 전체 원소 입력받기
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	// 이진 탐색 수행 결과 출력
+	int result = BinarySearch(arr, target, 0, n - 1);
+	if (result == -1)
+		cout << "원소가 존재하지 않습니다.\n";
+	else
+		cout << result + 1 << '\n';
+	return 0;
+}
