320110218c1f6338a8d151e201948e9337696316
DoHyun Kim <dohyun9565@gmail.com>
2021-01-16 23:23:21 +0900

    implement

---
+++ 2021_Algorithm/BOJ10539.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	//1000000000 10¾ï
+	int N;
+	cin >> N;
+	vector<long long> B(N, 0);
+	vector<long long> A(N, 0);
+	for (int i = 0; i < N; i++)
+		cin >> B[i];
+
+	for (int i = 1; i <= N; i++)
+	{
+		B[i - 1] = B[i - 1] * i;
+	}
+
+	A[0] = B[0];
+	for (int i = 1; i < N; i++)
+		A[i] = B[i] - B[i - 1];
+
+	for (int i = 0; i < N; i++)
+		cout << A[i] << ' ';
+
+
+	return 0;
+}
