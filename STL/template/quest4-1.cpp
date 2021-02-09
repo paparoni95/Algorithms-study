f6b871857152b5b35e5ca1e7317a2990d72c2c14
KimDoHyun <dohyun9565@gmail.com>
2021-02-09 19:28:02 +0900

    연습 문제

---
+++ STL/template/quest4-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class MyType{};
+
+template<typename T>
+void Copy(T t[], const T s[], int size)
+{
+	for (int i = 0; i < size; i++)
+		t[i] = s[i];
+}
+
+int main()
+{
+	int arr1[5] = { 10,20,30,40,50 };
+	int arr2[5];
+	Copy(arr2, arr1, 5);
+
+	MyType myArr1[5];
+	MyType myArr2[5];
+	Copy(myArr2, myArr1, 5);
+
+	return 0;
+}
---
+++ STL/template/quest4-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class Queue
+{
+	enum {CAP = 100};
+	T buf[CAP];
+	int front;
+	int rear;
+public:
+	Queue():front(0),rear(0){}
+	void Push(const T& data)
+	{
+		buf[rear = (rear + 1) % CAP] = data;
+	}
+	const T& Pop()
+	{
+		return buf[front = (front + 1) % CAP];
+	}
+	bool Empty() const
+	{
+		return rear == front;
+	}
+};
+
+int main()
+{
+	Queue<int> q;
+
+	q.Push(10);
+	q.Push(20);
+	q.Push(30);
+
+	if (!q.Empty())
+		cout << q.Pop() << endl;
+	if (!q.Empty())
+		cout << q.Pop() << endl;
+	if (!q.Empty())
+		cout << q.Pop() << endl;
+
+	return 0;
+}
