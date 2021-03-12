07113149ebe59d99d74a058123c2f0888848e16a
KimDoHyun <dohyun9565@gmail.com>
2021-03-12 12:05:47 +0900

    투 포인터 알고리즘

--- TechniqueSkill/TwoPointer.cpp
+++ TechniqueSkill/TwoPointer.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 /*
-	투 포인터 알고리즘 : 리스트에 순차적으로 접근해야 할 때
-	두 개의 점을 이용하여 위치를 기록하면서 처리하는 기법
+	# 투 포인터 알고리즘
+	리스트에 순차적으로 접근해야 할 때 2개의 점의 위치를 기록하면서 처리
 
-	O(N)
+	ex) 특정한 합을 가지는 부분 연속 수열 찾기 문제
 
-	부분 연속 수열의 합을 빠르게 찾고자할 때 사용하는 기법이다.
+	# 동작 방식
+	1. 시작점(start)과 끝점(end)이 첫 번째 원소의 인덱스를 가리키도록 한다.
+	2. 현재 부분합이 M과 같다면 카운트한다.
+	3. 현재 부분합이 M보다 작으면 end를 1 증가시킨다.
+	4. 현재 부분합이 M보다 크거나 같으면 start를 1 증가시킨다.
+	5. 모든 경우를 확인할 때까지 2번부터 4번까지의 과정을 반복한다.
 */
 
-int n = 5, m = 5; // 데이터의 개수, 부분 연속 수열의 합
-vector<int> v = { 1,2,3,2,5 };
+int n = 5; // 데이터의 개수
+int m = 5; // 찾고자 하는 부분합
+int arr[] = { 1,2,3,2,5 }; // 전체 수열
 
 int main()
 {
-	int sum = 0, cnt = 0;
-	for (int left = 0, right = 0; left < n; left++)
-	{
-		// right를 가능한 만큼 이동시키기
-		while (sum < m && right < n)
-		{
-			sum += v[right];
-			right++;
-		}
-		// 부분 합이 m일 때, 카운트 증가
-		if (sum == m) cnt++;
+	int cnt = 0;
+	int intervalSum = 0;
+	int end = 0;
 
-		sum -= v[left];
+	// start를 차례대로 증가시키며 반복
+	for (int start = 0; start < n; start++)
+	{
+		// end를 가능한 만큼 이동시키기
+		while (intervalSum < m && end < n)
+		{
+			intervalSum += arr[end];
+			end++;
+		}
+		// 부분합이 m일 때 카운트 증가
+		if (intervalSum == m) cnt++;
+		intervalSum -= arr[start];
 	}
 
-	cout << cnt;
+	cout << cnt << '\n';
 	return 0;
 }
