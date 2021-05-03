0854f6739c461a1074d3387ca9a8ccf4a4bd66c3
DoHyun Kim <dohyun9565@gmail.com>
2021-05-03 11:46:23 +0900

    implement - 같은 숫자는 싫어

--- 2021_Algorithm/Programmers/programmers12906.cpp
+++ 2021_Algorithm/Programmers/programmers12906.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-// 같은 숫자는 싫어(구현)
+
 vector<int> solution(vector<int> arr)
 {
     arr.erase(unique(arr.begin(), arr.end()), arr.end());
