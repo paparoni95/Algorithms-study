c46a0bb9156b05be193599246b285d573d5db5b2
DoHyun Kim <dohyun9565@gmail.com>
2021-05-03 11:46:07 +0900

    implement - 2016년

--- 2021_Algorithm/Programmers/programmers12901.cpp
+++ 2021_Algorithm/Programmers/programmers12901.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-// 2016³â(±¸Çö)
+
 vector<int> days = { 31,29,31,30,31,30,31,31,30,31,30,31 };
 vector<string> name = { "THU","FRI","SAT","SUN","MON","TUE","WED" };
 
