aaa38f2b255f9f21de8e7783c0a35787107751cd
DoHyun Kim <dohyun9565@gmail.com>
2021-05-11 15:31:12 +0900

    xor operator - 나머지 한 점

---
+++ 2021_Algorithm/Programmers/programmers1878.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> solution(vector<vector<int> > v) {
+    vector<int> ans(2, 0);
+
+    for (int i = 0; i < v.size(); i++) {
+        ans[0] ^= v[i][0];
+        ans[1] ^= v[i][1];
+    }
+
+    return ans;
+}
