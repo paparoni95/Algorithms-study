e60eaea91932cf9f758eb12f77dde8cbca7cb3b6
paparoni95 <dohyun9565@gmail.com>
2021-05-18 02:26:10 +0900

    modular - 124 나라의 숫자

---
+++ 2021_Algorithm/Programmers/programmers12899.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    char ch[3] = { '4','1','2' };
+    while (n) {
+        answer = ch[n % 3] + answer;
+        if (n % 3 == 0) n = n / 3 - 1;
+        else n /= 3;
+    }
+
+    return answer;
+}
