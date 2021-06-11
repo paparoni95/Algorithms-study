986a1bfebea9334083950d5a94331b616cdbd20e
DoHyun Kim <dohyun9565@gmail.com>
2021-06-12 03:32:36 +0900

    backtracking - N과 M(8)

---
+++ 2021_Algorithm/BaekJoon/BOJ15657.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int picked[8];
+void dfs(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << picked[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		picked[cnt] = v[i];
+		dfs(i, cnt + 1);
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	v.resize(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+	dfs(0, 0);
+	return 0;
+}
