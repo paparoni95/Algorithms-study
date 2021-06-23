0669dd8b6f99b74a4029db7d4f53ca0ca0b79962
paparoni95 <dohyun9565@gmail.com>
2021-06-23 20:38:47 +0900

    BFS

--- 2021_Algorithm/BOJ1600.cpp
+++ 2021_Algorithm/BOJ1600.cpp
@@ -1,91 +1,70 @@
 #include <iostream>
 #include <queue>
-#include <cstring>
+#include <algorithm>
 using namespace std;
 
-const int horseX[] = { -1,-2,-2,-1,1,2,2,1 };
-const int horseY[] = { -2,-1,1,2,-2,-1,1,2 };
-const int dx[] = { 0,0,-1,1 };
-const int dy[] = { 1,-1,0,0 };
+int W, H, K;
+int maze[200][200];
+bool vis[200][200][31];
 
-int k, r, c;
-int a[201][201];
-int dist[31][201][201]; // (x,y)에서 k번 능력을 사용해서 온 비용
+int hx[] = { -1,-2,-2,-1,1,2,2,1 };
+int hy[] = { -2,-1,1,2,-2,-1,1,2 };
+int dx[] = { -1,1,0,0 };
+int dy[] = { 0,0,-1,1 };
 
-struct Monkey
-{
-	int x, y, used;
+struct Monkey {
+	int x, y, k, cost;
 };
 
-void input()
-{
-	cin >> k >> c >> r;
-	for (int i = 0; i < r; i++)
-	{
-		for (int j = 0; j < c; j++)
-		{
-			cin >> a[i][j];
-		}
-	}
-
-	memset(dist, -1, sizeof(dist));
-}
-
-int bfs()
-{
+int move() {
 	queue<Monkey> q;
-	q.push({ 0,0,0 });
-	dist[0][0][0] = 0;
-
-	while (!q.empty())
-	{
+	q.push({ 0,0,0,0 });
+	vis[0][0][0] = 0;
+	while (!q.empty()) {
 		Monkey cur = q.front();
 		q.pop();
 
-		if (cur.x == r - 1 && cur.y == c - 1) return dist[cur.used][cur.x][cur.y];
+		if (cur.x == H - 1 && cur.y == W - 1)
+			return cur.cost;
 
-		if (cur.used < k) // 능력이 남았다면 사용하기
-		{
-			for (int i = 0; i < 8; i++) // 능력을 사용했을 때
-			{
-				int nx = cur.x + horseX[i];
-				int ny = cur.y + horseY[i];
-				if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
-				if (dist[cur.used + 1][nx][ny] == -1 && a[nx][ny] == 0)
-				{
-					dist[cur.used + 1][nx][ny] = dist[cur.used][cur.x][cur.y] + 1;
-					q.push({ nx,ny,cur.used + 1 });
+		if (cur.k < K) {
+			for (int i = 0; i < 8; i++) {
+				int nx = cur.x + hx[i];
+				int ny = cur.y + hy[i];
+				if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+				if (maze[nx][ny]) continue;
+				if (vis[nx][ny][cur.k + 1] == false) {
+					vis[nx][ny][cur.k + 1] = true;
+					q.push({ nx,ny,cur.k + 1,cur.cost + 1 });
 				}
 			}
 		}
 
-		for (int i = 0; i < 4; i++) // 능력을 사용하지 않을 때
-		{
+		for (int i = 0; i < 4; i++) {
 			int nx = cur.x + dx[i];
 			int ny = cur.y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
-			if (dist[cur.used][nx][ny] == -1 && a[nx][ny] == 0)
-			{
-				dist[cur.used][nx][ny] = dist[cur.used][cur.x][cur.y] + 1;
-				q.push({ nx,ny,cur.used });
+			if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+			if (maze[nx][ny]) continue;
+			if (vis[nx][ny][cur.k] == false) {
+				vis[nx][ny][cur.k] = true;
+				q.push({ nx,ny,cur.k,cur.cost + 1 });
 			}
 		}
 	}
 
 	return -1;
 }
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-void solve()
-{
-	input();
-	int ret = bfs();
-	cout << ret << '\n';
-}
+	cin >> K >> W >> H;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			cin >> maze[i][j];
+		}
+	}
 
-int main()
-{
-	freopen("input.txt", "r", stdin);
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	solve();
+	cout << move() << '\n';
 	return 0;
 }
--- 2021_Algorithm/BOJ7569.cpp
+++ 2021_Algorithm/BOJ7569.cpp
@@ -1,137 +1,89 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
+int M, N, H;
+int maze[101][101][101];
+int vis[101][101][101];
+int dx[] = { -1,1,0,0,0,0 };
+int dy[] = { 0,0,-1,1,0,0 };
+int dz[] = { 0,0,0,0,1,-1 };
 
-struct Pos
-{
+struct Pos {
 	int z, x, y;
 };
-const int MAX = 101;
-const int dz[] = { 0,0,0,0,1,-1 };
-const int dx[] = { 0,0,1,-1,0,0 };
-const int dy[] = { 1,-1,0,0,0,0 };
 
-int board[MAX][MAX][MAX];
-bool visit[MAX][MAX][MAX];
-int M, N, H; // 가로, 세로, 높이
-
-queue<Pos> q;
-
-void print()
-{
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			for (int k = 0; k < M; k++)
-			{
-				cout << board[i][j][k];
-			}
-			cout << endl;
-		}
-		cout << endl;
-	}
-}
-
-void BFS() // 토마토 익히기
-{
-	while (!q.empty())
-	{
-		int cz = q.front().z;
-		int cx = q.front().x;
-		int cy = q.front().y;
-		q.pop();
-
-		for (int i = 0; i < 6; i++)
-		{
-			int nz = cz + dz[i];
-			int nx = cx + dx[i];
-			int ny = cy + dy[i];
-			if (nz < 0 || nx < 0 || ny < 0 || nz >= H || nx >= N || ny >= M) continue;
-			if (board[nz][nx][ny] != 0 || visit[nz][nx][ny]) continue;
-
-			visit[nz][nx][ny] = true;
-			board[nz][nx][ny] = board[cz][cx][cy] + 1;
-			q.push({ nz,nx,ny });
-		}
-	}
-}
-
-bool checkTomato() // 토마토가 모두 익었다면 true, 아니면 false
-{
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			for (int k = 0; k < M; k++)
-			{
-				if (!board[i][j][k] && !visit[i][j][k])
-					return false;
+void init() {
+	for (int i = 0; i < 101; i++) {
+		for (int j = 0; j < 101; j++) {
+			for (int k = 0; k < 101; k++) {
+				vis[i][j][k] = -1;
 			}
 		}
 	}
-	return true;
 }
 
-int calculateTomato() // 토마토가 모두 익을 때까지 며칠 걸리는지 반환
-{
-	int day = 0;
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			for (int k = 0; k < M; k++)
-			{
-				day = max(day, board[i][j][k]);
-			}
-		}
-	}
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	return day;
-}
-
-int main()
-{
-	freopen("input.txt", "r", stdin);
-	ios_base::sync_with_stdio(0); cin.tie(0);
-
+	init();
 	cin >> M >> N >> H;
-	bool check = false;
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			for (int k = 0; k < M; k++)
-			{
-				cin >> board[i][j][k];
-
-				if (board[i][j][k] == 1)
-				{
-					q.push({ i,j,k, });
-					board[i][j][k] = 0;
-					visit[i][j][k] = true;
+	int zeroTomato = 0;
+	queue<Pos> q;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < M; k++) {
+				cin >> maze[i][j][k];
+				if (maze[i][j][k] == 1) {
+					q.push({ i,j,k });
+					vis[i][j][k] = 1;
 				}
-					
-				else if (!board[i][j][k])
-					check = true;
+				else if (maze[i][j][k] == 0) {
+					zeroTomato++;
+				}
 			}
 		}
 	}
 
-	// 토마토가 처음부터 모두 익었다면
-	if (!check)
-		cout << 0;
-	else
-	{
-		BFS();
-		if (checkTomato())
-			cout << calculateTomato();
-		else
-			cout << -1;
+	if (zeroTomato == 0) {
+		cout << "0\n";
+		return 0;
 	}
-		
+
+	while (!q.empty()) {
+		int sz = q.size();
+		for (int tomato = 0; tomato < sz; tomato++) {
+			Pos cur = q.front();
+			q.pop();
+
+			for (int i = 0; i < 6; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				int nz = cur.z + dz[i];
+				if (nx < 0 || ny < 0 || nz < 0 || nx >= N || ny >= M || nz >= H) continue;
+				if (maze[nz][nx][ny] == 0 && vis[nz][nx][ny] == -1) {
+					vis[nz][nx][ny] = vis[cur.z][cur.x][cur.y] + 1;
+					maze[nz][nx][ny] = 1;
+					q.push({ nz,nx,ny });
+				}
+			}
+		}
+	}
+
+	int day = 0;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < M; k++) {
+				if (maze[i][j][k] == 0) {
+					cout << "-1\n";
+					return 0;
+				}
+				day = max(day, vis[i][j][k]);
+			}
+		}
+	}
+
+	cout << day - 1 << '\n';
 	return 0;
 }
